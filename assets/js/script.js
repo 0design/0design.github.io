5c20ff10135888697dc5ecb336f8db5f5fcc0977
gyanl <gyanlakhwani@gmail.com>
2016-10-12 03:00:04 +0530

    Add favicon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,45 @@
-if(function(e,t,n,o,l,d,c){e.GoogleAnalyticsObject=l,e[l]=e[l]||function(){(e[l].q=e[l].q||[]).push(arguments)},e[l].l=1*new Date,d=t.createElement(n),c=t.getElementsByTagName(n)[0],d.async=1,d.src=o,c.parentNode.insertBefore(d,c)}(window,document,"script","https://www.google-analytics.com/analytics.js","ga"),ga("create","UA-80883070-1","auto"),ga("send","pageview"),-1!=document.querySelector("body").className.indexOf("index"))var win=window,docEl=document.documentElement,$logo=document.getElementById("dis");var lastScrollTop=0;window.addEventListener("scroll",function(){var e=window.pageYOffset||document.documentElement.scrollTop;document.documentElement.clientWidth<768&&(0==e?document.getElementById("downheader").style.top="-52px":e>lastScrollTop?document.getElementById("downheader").style.top="-52px":(document.getElementById("downheader").style.visibility="visible",document.getElementById("blogheader").style.visibility="visible",document.getElementById("downheader").style.top="0px")),lastScrollTop=e},!1);
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-80883070-1', 'auto');
+    ga('send', 'pageview');
+
+    if (document.querySelector('body').className.indexOf('index') != -1){
+
+  var win = window,
+      docEl = document.documentElement,
+      $logo = document.getElementById('dis');
+
+     win.onscroll = function(){
+     var sTop = (this.pageYOffset || docEl.scrollTop)  - (docEl.clientTop || 0);
+     $logo.style.fontSize =  sTop > 10 ? "0":"32px" ;
+  };
+}
+
+
+
+  var lastScrollTop = 0;
+  // element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+  window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
+     var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+
+     if (document.documentElement.clientWidth < 768) {
+         // some code..
+     if (st == 0){
+       //downscroll
+       document.getElementById('downheader').style.top = "-52px";
+     } else if (st > lastScrollTop){
+       //downscroll
+       document.getElementById('downheader').style.top = "-52px";
+
+     } else {
+       //upscroll
+       document.getElementById('downheader').style.visibility = "visible";
+       document.getElementById('blogheader').style.visibility = "visible";
+       document.getElementById('downheader').style.top = "0px";
+     }
+   }
+     lastScrollTop = st;
+  }, false);
